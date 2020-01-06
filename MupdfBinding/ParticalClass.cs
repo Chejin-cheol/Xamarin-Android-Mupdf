65e5a82e75c31c6fcfb70aab3b9a45a1a317cbf4
jklnm888 <jklnm888@gmail.com>
2020-01-06 17:50:45 +0900

    init

---
+++ MupdfBinding/ParticalClass.cs
@@ -0,0 +1,40 @@
+﻿using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+
+namespace Com.Artifex.MuPdfDemo
+{
+    partial class ReaderView
+    {
+        // We use GetAdapter(...) / SetAdapter created in Metadata.xml
+        // instead of the missing Adapter { get; set; }
+        protected override Java.Lang.Object RawAdapter
+        {
+            get { return GetAdapter().JavaCast<Java.Lang.Object>(); }
+            set
+            {
+                var adapter = value.JavaCast<global::Android.Widget.IAdapter>();
+                this.SetAdapter(adapter);
+            }
+        }
+
+
+        // 커스텀
+
+/*
+  public void ApplyToChildren(ViewMapper mapper)
+  {
+
+  }
+
+  public abstract partial class ViewMapper
+  {
+     public abstract void ApplyToView(View view);
+  }
+  */
+
+    }
+
+
+}
